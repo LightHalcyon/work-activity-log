b5b727ad489a2bbbe470727cbd9f757d3d023d34
Adityawarman Fanaro <adityawarman.fanaro@outlook.com>
2018-07-17 20:46:35 +0700

    Initialize Spring Boot

---
+++ src/main/java/com/xeus/worklog/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.xeus.worklog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
